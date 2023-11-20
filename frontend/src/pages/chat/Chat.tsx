ef04a0fe9c07c844f4f89cafadf1f02bcde5b94f
Enrico Schincariol <enrico.schincariol@adaptfy.com>
2023-11-20 16:01:53 +0100

    changed logo, chatbot assistant text

--- frontend/src/pages/chat/Chat.tsx
+++ frontend/src/pages/chat/Chat.tsx
@@ -579,8 +579,8 @@ const Chat = () => {
                                     className={styles.chatIcon}
                                     aria-hidden="true"
                                 />
-                                <h1 className={styles.chatEmptyStateTitle}>Start chatting</h1>
-                                <h2 className={styles.chatEmptyStateSubtitle}>This chatbot is configured to answer your questions</h2>
+                                <h1 className={styles.chatEmptyStateTitle}>Ask me anything about your SHV data!</h1>
+                                <h2 className={styles.chatEmptyStateSubtitle}>Answers are based on documents provided on SharePoint.</h2>
                             </Stack>
                         ) : (
                             <div className={styles.chatMessageStream} style={{ marginBottom: isLoading ? "40px" : "0px"}} role="log">
